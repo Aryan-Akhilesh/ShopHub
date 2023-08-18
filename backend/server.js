2ab922844dff7a9b1161c4fdd13d85c60a248b09
Aryan Akhilesh <aryanakhil99@gmail.com>
2023-08-18 13:15:25 -0400

    connect to MongoDB using mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import products from "./data/products.js";
+import connectDB from "./config/db.js";
+
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 8000;
